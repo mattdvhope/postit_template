2794e9accdf929e151d7e32c237e3253fda25659
Matt Malone <mattagape@gmail.com>
2013-12-02 21:54:45 -0500

    altered models

--- app/models/category.rb
+++ app/models/category.rb
@@ -1,3 +1,4 @@
 class Category < ActiveRecord::Base
+  has_many :subjects
   has_many :posts, :through => :subjects
 end
--- app/models/post.rb
+++ app/models/post.rb
@@ -1,5 +1,8 @@
 class Post < ActiveRecord::Base
   belongs_to :user
   has_many :comments
-  has_many :categories :through => :subjects
+  has_many :subjects
+  has_many :categories, :through => :subjects
 end
+
+

d3259d8409c60472ca2b833fdcceb150f8a1167b
Matt Malone <mattagape@gmail.com>
2013-12-21 17:16:52 -0500

    changes

--- app/helpers/application_helper.rb
+++ app/helpers/application_helper.rb
@@ -9,6 +9,10 @@ module ApplicationHelper
   end
 
   def display_datetime(datetime)
+    if logged_in? && !current_user.time_zone.blank?
+      datetime = datetime.in_time_zone(current_user.time_zone) # this enables the resetting of the datetime zone for that current user; the timezones of the other users are thus converted to the user's time_zone
+    end
+
     datetime.strftime("%m/%d/%Y %l:%M%P %Z")
   end
 

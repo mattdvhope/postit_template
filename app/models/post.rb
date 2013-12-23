b001d0de0667876dec3423bb9e4901a3f0b7990c
Matt Malone <mattagape@gmail.com>
2013-12-23 16:09:54 -0500

    changes

--- app/models/post.rb
+++ app/models/post.rb
@@ -7,13 +7,13 @@ class Post < ActiveRecord::Base
   has_many :categories, through: :subjects
 # has_many :votes, as: :voteable ..has been extracted to lib/voteable.rb
 
-  after_validation :generate_slug #We're using the 'after_validation' (ActiveRecord Callback) method to generate the slug after validation, but before .save ; This implies that our 'generate_slug' method will be executed even for updates; 'before_save' callback method could also work
+  before_save :generate_slug #We're using the 'after_validation' (ActiveRecord Callback) method to generate the slug after validation, but before .save ; This implies that our 'generate_slug' method will be executed even for updates; 'after_validation' callback method could also work
 
   validates :title, presence: true, length: {minimum: 5}
   validates :description, presence: true
   validates :url, presence: true, uniqueness: true
 
-  def generate_slug
+  def generate_slug #.slug method comes from the 'slug' column(virtual attribute) in the 'posts' table.
     self.slug = self.title.downcase.gsub(/[\s.\/_]/, ' ').squeeze(' ').strip.gsub(/[^\w-]/, '').tr(' ', '-') #this is set as a variable, but is not yet saved to the DB. Thus, you'll need '.save' in the next line; but better yet, you can save it to the ActiveRecord call-back.
   end
 
